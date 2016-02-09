80e7cd3dda2e8d515554a533de1c506cf5be4852
Dan Scroggins <Dan Scroggins@Heidelberg.local>
2016-02-09 09:14:03 -0500

    Updating script

--- computer_cleanup.ps1
+++ computer_cleanup.ps1
@@ -1,20 +1,28 @@
-﻿#$creds = Get-Credential
-$pc = Get-ADComputer -Filter * -SearchBase "cn=computers,dc=heidelberg,dc=local"
+﻿$pc = Get-ADComputer -Filter * -SearchBase "cn=computers,dc=heidelberg,dc=local"
 foreach ($i in $pc) {
 $name = $i.Name
 $dn = $i.DistinguishedName
-$r = Test-Connection $name -Count 1 | select Address,IPV4Address
-$ips = $r.IPV4Address.IPAddressToString
+$ip = (nslookup $name)[4]
+$ips = $ip.Trim("Address:  ")
+$ips
 switch -wildcard ($ips)
     { 
         "10.23.2*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Dayton,DC=Heidelberg,DC=local"}
+        "10.23.142*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Dayton,DC=Heidelberg,DC=local"}
         "10.23.7*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Cleveland,DC=Heidelberg,DC=local"}
+        "10.23.137*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Cleveland,DC=Heidelberg,DC=local"}
         "10.23.5*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Ohio Valley,DC=Heidelberg,DC=local"}
+        "10.23.105*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Ohio Valley,DC=Heidelberg,DC=local"}
         "10.23.10*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Hebron,DC=Heidelberg,DC=local"}
+        "10.23.110*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Hebron,DC=Heidelberg,DC=local"}
         "10.23.8*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Toledo,DC=Heidelberg,DC=local"}
+        "10.23.108*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Toledo,DC=Heidelberg,DC=local"}
         "10.23.3*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Lorain,DC=Heidelberg,DC=local"}
+#Wireless "10.23.3*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Lorain,DC=Heidelberg,DC=local"}
         "10.23.9*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Columbus,DC=Heidelberg,DC=local"}
+        "10.23.109*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Columbus,DC=Heidelberg,DC=local"}
         "10.23.1*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Cincinnati,DC=Heidelberg,DC=local"}
+        "10.23.101*" {Move-ADObject -Identity "$dn" -TargetPath "OU=Computers,OU=Cincinnati,DC=Heidelberg,DC=local"}
         default {Write-Host "Computer location could not be determined. Leaving in Computers OU"}
     }
 }
